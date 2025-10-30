4d6dc062d7bf2b37b5683cc0010c1070df884e01
shanaiamau99-lgtm <shanaiamau99@gmail.com>
2025-10-30 21:37:33 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // app.js (module)
-export const SUPABASE_URL = "https://YOURPROJECT.supabase.co";      // <-- paste
-export const SUPABASE_ANON_KEY = "YOUR_ANON_PUBLIC_KEY";            // <-- paste
+export const SUPABASE_URL = "https://qjnusrvcnlpawpsphsll.supabase.co";      // <-- paste
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqbnVzcnZjbmxwYXdwc3Boc2xsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4Mjc0NzMsImV4cCI6MjA3NzQwMzQ3M30.xulwKxzyzlflUW6nORWN58QsVvHsh0uVp9tbrdNfACc";            // <-- paste
 
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
